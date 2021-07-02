36547e786aa63fb7e72b764a1733d9a8281063da
Safvan ct <safvanct@mindbare.com>
2021-07-02 17:20:01 +0530

    INDIA  data updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,11 @@ import { Navbar, Nav } from 'react-bootstrap';
 function App() {
     return ( 
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand><Link to="/" className="mr-3 text-white text-decoration-none">Covid -19 </Link></Navbar.Brand>
+            <Navbar.Brand>
+                <Link to="/" className="mr-3 text-white text-decoration-none">
+                    <img src={process.env.PUBLIC_URL + "/logo.jpg"} alt="logo" style={{ width: "100px"}}/>
+                </Link>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
--- src/components/India.js
+++ src/components/India.js
@@ -1,70 +1,84 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from 'react-bootstrap';
 import StateInfo from './StateInfo';
+import React, { Component } from 'react';
+import Axios from 'axios';
 
-function India() {
-    return ( 
-        <div className="container">
-            <div className=" row mt-3">
-                <div className="col-md-12 mb-3">
-                    <img src="https://www.countryflags.io/in/shiny/64.png" alt="India"/>
-                    <h3>INDIA</h3>
-                </div>
+class India extends Component {
 
-                <div className="col-md-3 mb-3">
-                    <Card className="bg-primary">
-                        <Card.Body className="text-white text-center">
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
+    constructor() {
+        super();
+        this.state = {
+            indiaData : {}
+        }
+    }
 
-                <div className="col-md-3 mb-3">
-                    <Card className="bg-warning">
-                        <Card.Body className="text-white text-center">
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
+    componentDidMount() {
+        Axios.get('https://corona.lmao.ninja/v2/countries/india').then(response=>{
+            this.setState({indiaData:response.data});
+        });
+    };
 
-                <div className="col-md-3 mb-3">
-                    <Card className="bg-success">
-                        <Card.Body className="text-white text-center">
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
+    render() {
+        return ( 
+            <div className="container">
+                <div className=" row mt-3">
+                    <div className="col-md-12 mb-3">
+                        <img src="https://www.countryflags.io/in/shiny/64.png" alt="India"/>
+                        <h3>INDIA</h3>
+                    </div>
 
-                <div className="col-md-3 mb-3">
-                    <Card className="bg-danger">
-                        <Card.Body className="text-white text-center">
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
+                    <div className="col-md-3 mb-3">
+                        <Card className="bg-primary">
+                            <Card.Body className="text-white text-center">
+                                <Card.Title>TOTAL CASES</Card.Title>
+                                <Card.Text>
+                                    { this.state.indiaData.cases }
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
 
-                <div className="col-md-12 mb-3">
-                    <StateInfo/>
+                    <div className="col-md-3 mb-3">
+                        <Card className="bg-warning">
+                            <Card.Body className="text-white text-center">
+                                <Card.Title>ACTIVE CASES</Card.Title>
+                                <Card.Text>
+                                    { this.state.indiaData.active }
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+
+                    <div className="col-md-3 mb-3">
+                        <Card className="bg-success">
+                            <Card.Body className="text-white text-center">
+                                <Card.Title>RECOVERED</Card.Title>
+                                <Card.Text>
+                                    { this.state.indiaData.recovered }
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+
+                    <div className="col-md-3 mb-3">
+                        <Card className="bg-danger">
+                            <Card.Body className="text-white text-center">
+                                <Card.Title>DEATH</Card.Title>
+                                <Card.Text>
+                                    { this.state.indiaData.deaths }
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+
+                    <div className="col-md-12 mb-3">
+                        <StateInfo/>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default India;
